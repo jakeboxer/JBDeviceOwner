e816da493c37c518895ee8236f619d40ca4e6557
Jake Boxer <jake@github.com>
2012-04-03 19:15:36 -0400

    first commit

---
+++ JBDeviceOwner/JBDeviceOwner.h
@@ -0,0 +1,13 @@
+//
+//  JBDeviceOwner.h
+//  JBDeviceOwner
+//
+//  Created by Jacob Boxer on 4/3/12.
+//  Copyright (c) 2012 GitHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JBDeviceOwner : NSObject
+
+@end
---
+++ JBDeviceOwner/JBDeviceOwner.m
@@ -0,0 +1,13 @@
+//
+//  JBDeviceOwner.m
+//  JBDeviceOwner
+//
+//  Created by Jacob Boxer on 4/3/12.
+//  Copyright (c) 2012 GitHub. All rights reserved.
+//
+
+#import "JBDeviceOwner.h"
+
+@implementation JBDeviceOwner
+
+@end
